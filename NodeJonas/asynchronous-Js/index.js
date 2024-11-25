9e60a75d9e56fc815736634f16fd32dcf4288743
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-25 22:20:23 +0000

    Nov 25, 2024, 10:20 PM

--- NodeJonas/asynchronous-Js/index.js
+++ NodeJonas/asynchronous-Js/index.js
@@ -5,6 +5,8 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+        if (err) return console.log(err.message);
+        
         console.log(res.body.message);
 
         fs.writeFile('dog-img.txt', res.body.message, err => {
