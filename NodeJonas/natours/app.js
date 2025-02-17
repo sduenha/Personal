b917ad626e99b07048e34f38005a392bcff6cfe8
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-02-17 22:00:32 +0000

    modified:   NodeJonas/natours/app.js

--- NodeJonas/natours/app.js
+++ NodeJonas/natours/app.js
@@ -66,6 +66,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
         data: {
             tour: '<Updated tour here...>'
         }
+    });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if (req.params.id * 1 > tours.length) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'invalid ID'
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
     })
 })
 
