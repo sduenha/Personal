421d6de869a9d7e2d82eeb0750f1b94bb8a64342
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-08-28 13:08:24 +0000

    Aug 28, 2025, 1:08 PM

--- CursoEmVideo/java-poo/aula05/Aula.java
+++ CursoEmVideo/java-poo/aula05/Aula.java
@@ -2,9 +2,12 @@ public class Aula {
     public static void main(String[] args) {
         ContaBanco p1 = new ContaBanco();
         p1.abrirConta(0001, "CC", "Jubileu");
+        p1.depositar(100);
 
         ContaBanco p2 = new ContaBanco();
         p2.abrirConta(0002, "CP", "Creuza");
+        p2.depositar(500);
+        p2.sacar(100);
 
         p1.estadoAtual();
         p2.estadoAtual();
