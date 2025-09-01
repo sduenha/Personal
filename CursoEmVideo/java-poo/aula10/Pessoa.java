eba23f2aeea60bb762e7804246b87ce03fb582e4
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-09-01 14:22:28 +0000

    Sep 1, 2025, 2:22 PM

--- CursoEmVideo/java-poo/aula10/Pessoa.java
+++ CursoEmVideo/java-poo/aula10/Pessoa.java
@@ -9,5 +9,27 @@ public class Pessoa {
         this.sexo = s;
     }
 
-    
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String n) {
+        this.nome = n;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int i) {
+        this.idade = i;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String s) {
+        this.sexo = s;
+    }
 }
