b3e68b14c071de498b445d48cc2c1a02e1989b18
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-09-01 13:59:44 +0000

    Sep 1, 2025, 1:59 PM

--- CursoEmVideo/java-poo/aula09/Aula.java
+++ CursoEmVideo/java-poo/aula09/Aula.java
@@ -10,7 +10,9 @@ public class Aula {
         l[1] = new Livro("POO para Iniciantes", "Pedro Paulo", 500, p[1]);
         l[2] = new Livro("Java Avancado", "Maria Candido", 800, p[0]);
 
+        l[0].folhear(20);
         l[0].detalhes();
+        l[1].avancarPag();
         l[1].detalhes();
         l[2].detalhes();
     }
