59fd204a581247f050dc6040c507b78de02a1d36
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-09-04 13:00:27 +0000

    Sep 4, 2025, 1:00 PM

--- CursoEmVideo/java-poo/aula13/Cachorro.java
+++ CursoEmVideo/java-poo/aula13/Cachorro.java
@@ -3,4 +3,20 @@ public class Cachorro extends Lobo {
     public void emitirSom() {
         System.out.println("Au! Au! Au!");
     }
+
+    public void reagir(String frase) {
+
+    }
+
+    public void reagir(int hora, int min) {
+
+    }
+
+    public void reagir(Boolean dono) {
+
+    }
+
+    public void reagir(int idade, float peso) {
+        
+    }
 }
