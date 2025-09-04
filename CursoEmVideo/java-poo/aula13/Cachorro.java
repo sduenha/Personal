d8050abca0a7d2f95910251badb85d79676b7cf6
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-09-04 13:06:00 +0000

    Sep 4, 2025, 1:06 PM

--- CursoEmVideo/java-poo/aula13/Cachorro.java
+++ CursoEmVideo/java-poo/aula13/Cachorro.java
@@ -5,18 +5,40 @@ public class Cachorro extends Lobo {
     }
 
     public void reagir(String frase) {
-
+        if (frase.equals("Toma comida") || frase.equals("Ola")) {
+            System.out.println("Abanar e latir");
+        } else {
+            System.out.println("Rosnar");
+        }
     }
 
     public void reagir(int hora, int min) {
-
+        if (hora < 12) {
+            System.out.println("Abanar");
+        } else if (hora >= 18) {
+            System.out.println("Ignorar");
+        } else {
+            System.out.println("Abanar e latir");
+        }
     }
 
-    public void reagir(Boolean dono) {
-
+    public void reagir(boolean dono) {
+        System.out.println(dono ? "Abanar" : "Rosnar e Latir");
     }
 
     public void reagir(int idade, float peso) {
-        
+        if (idade < 5) {
+            if (peso < 10) {
+                System.out.println("Abanar");
+            } else {
+                System.out.println("Latir");
+            }
+        } else {
+            if (peso < 10) {
+                System.out.println("Rosnar");
+            } else {
+                System.out.println("Ignorar");
+            }
+        }
     }
 }
